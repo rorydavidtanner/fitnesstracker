4928593e247b480abb0fb330986299198a9d47c3
Rory Tanner <roryscoding@gmail.com>
2021-01-06 16:44:50 +1100

    update code to routes files

--- routes/api.js
+++ routes/api.js
@@ -32,4 +32,66 @@ router.get("/api/fitness", (req, res) => {
     });
 });
 
+router.post("/api/workouts", ({ body }, res) => {
+  Exercise.create(body)
+    .then(dbFitness => {
+      res.json(dbFitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts/bulk", ({ body }, res) => {
+  Exercise.insertMany(body)
+    .then(dbFitness => {
+      res.json(dbFitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Exercise.find({})
+    .sort({ date: -1 })
+    .then(dbFitness => {
+      res.json(dbFitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/exercise", ({ body }, res) => {
+  Exercise.create(body)
+    .then(dbFitness => {
+      res.json(dbFitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/exercise/bulk", ({ body }, res) => {
+  Exercise.insertMany(body)
+    .then(dbFitness => {
+      res.json(dbFitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/exercise", (req, res) => {
+  Exercise.find({})
+    .sort({ date: -1 })
+    .then(dbFitness => {
+      res.json(dbFitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 module.exports = router;
